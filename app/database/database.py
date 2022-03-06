b326fb7683955788f16bda4919c8c5b399c7a55e
hyeonproject <axzswq@gmail.com>
2022-03-07 03:55:30 +0900

    update: Multi OS DB Connection

--- app/database/database.py
+++ app/database/database.py
@@ -1,13 +1,24 @@
+import platform
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import QueuePool
 
-SQLALCHEMY_DATABASE_URL = 'mysql+mysqldb://hyeonproject:hyeonproject@127.0.0.1/auth'
+SQLALCHEMY_DATABASE_URL_1 = 'mysql+mysqldb://hyeonproject:hyeonproject@127.0.0.1/auth'
+SQLALCHEMY_DATABASE_URL_2 = 'postgresql+psycopg2://auth:authproject@127.0.0.1:5432/auth'
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, echo=True, future=True, pool=QueuePool, pool_size=10
-)
+os_name = platform.system()
+
+if os_name == 'Windows':
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL_2, echo=True, future=True, pool=QueuePool, pool_size=10
+    )
+else:
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL_1, echo=True, future=True, pool=QueuePool, pool_size=10
+    )
+
 
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
